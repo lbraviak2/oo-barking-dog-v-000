114d117deaadba55b4e509dd259d4cf5cee658d1
Lindsay Braviak <lbraviak@gmail.com>
2018-07-06 19:46:49 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Dog
+  def initialize(name,bark = "woof")
+    @name = name
+    @bark = bark
+  end
+
+  def name=(dogs_name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def bark
+    @bark
+  end
+
+end
